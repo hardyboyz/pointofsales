5917aef1b2d9bafd31b5c26618689142640c7829
hardi <hardi84@gmail.com>
2017-02-13 15:34:26 +0700

    First

---
+++ application/azkasir/default/views/v_footer.php
@@ -0,0 +1,9 @@
+					</div> <!-- end content -->
+				</div> <!-- end col-md-12 -->
+			</div> <!-- end row -->
+		</div>
+		<footer>
+			Copyright &copy; 2016 <a href="http://digitalbarkah.com" target="_blank">Education & Computer Centre</a>
+		</footer>
+    </body>
+</html>
